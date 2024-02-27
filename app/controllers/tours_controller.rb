74e49aa5dbb64bfd964f171bd3b2cbb2eb04a757
Teodor Rothman <106080782+teorothman@users.noreply.github.com>
2024-02-27 18:04:17 +0100

    added a search bar with city functionality

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,5 +1,9 @@
 class ToursController < ApplicationController
   def index
     @tours = Tour.all
+
+    if params[:search_by_city] && params[:search_by_city] != ""
+      @tours = @tours.select { |n| n.location.downcase == params[:search_by_city].downcase}
+    end
   end
 end
