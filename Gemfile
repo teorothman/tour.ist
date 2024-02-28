ff9e3afca3a9290b4fcc8961eed2bd4877c71109
Laura Fernandez <laurafcavia@gmail.com>
2024-02-28 15:08:44 +0100

    cleaning seeds

--- Gemfile
+++ Gemfile
@@ -77,3 +77,5 @@ end
 
 # Handles the users
 gem "devise"
+
+gem 'faker', '~> 3.2'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 #Drop db
 
 puts "Dropping current data"
@@ -10,23 +12,28 @@ User.destroy_all
 puts "creating three categories"
 categories = []
 
-cat_1 = Category.create! :title => 'Walking', :description => 'Walking tour that requires... Walking'
+cat_1 = Category.create! title: 'Walking', description: 'Walking tour that requires... Walking'
 categories << cat_1
-cat_2 = Category.create! :title => 'Segway', :description => 'Segway tour for the adve... lazy!'
+cat_2 = Category.create! title: 'Segway', description: 'Segway tour for the adve... lazy!'
 categories << cat_2
-cat_3 = Category.create! :title => 'Biking', :description => 'For the ones who want to explore a lot!!!'
+cat_3 = Category.create! title: 'Biking', description: 'For the ones who want to explore a lot!!!'
 categories << cat_3
 
 
 # Creating 10 users with 2 tours each
+# 🔴 email: "#{:first_name}@gmail.com",
 puts "creating 10 users with 2 tours each"
 
-num = 0
 languages = ["Spanish", "English", "French", "Swedish"]
 locations = ["Barceona", "Madrid", "Tolouse", "Stockholm", "Buenos Aires"]
 10.times do
-  user = User.create! :first_name => "john#{num}", :last_name => "doe#{num}", :email => "john#{num}@gmail.com", :password => 'topsecret', :password_confirmation => 'topsecret'
-  num += 1
+  user = User.create! first_name: Faker::Name.unique.first_name,
+                      last_name: Faker::Name.unique.last_name,
+                      # email: Faker::Internet.unique.email,
+                      email: "#{:first_name}@gmail.com",
+                      password: 'topsecret',
+                      password_confirmation: 'topsecret'
+
   2.times do
     Tour.create! :title => 'Awesome Title', :description => 'Awesome description', :duration => rand(30..90), :max_spots => rand(1..100), :price_per_person => rand(10..100), :date => Date.tomorrow, :category_id => categories[rand(0..2)].id, :language => languages[rand(0..3)], :location => locations[rand(0..4)], :user_id => user.id
   end
