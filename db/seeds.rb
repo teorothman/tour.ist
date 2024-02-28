88c8ca45ac3426da882d5fb3bce32657d4e2613a
Laura Fernandez <laurafcavia@gmail.com>
2024-02-28 15:25:51 +0100

    seed cleaning done

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,26 +16,29 @@ cat_1 = Category.create! title: 'Walking', description: 'Walking tour that requi
 categories << cat_1
 cat_2 = Category.create! title: 'Segway', description: 'Segway tour for the adve... lazy!'
 categories << cat_2
-cat_3 = Category.create! title: 'Biking', description: 'For the ones who want to explore a lot!!!'
+cat_3 = Category.create! title: 'Biking', description: 'Tour for the ones who want to explore a lot!!!'
 categories << cat_3
 
 
 # Creating 10 users with 2 tours each
-# 🔴 email: "#{:first_name}@gmail.com",
 puts "creating 10 users with 2 tours each"
 
 languages = ["Spanish", "English", "French", "Swedish"]
 locations = ["Barceona", "Madrid", "Tolouse", "Stockholm", "Buenos Aires"]
 10.times do
-  user = User.create! first_name: Faker::Name.unique.first_name,
+  f_name = Faker::Name.unique.first_name
+
+  user = User.create! first_name: f_name,
                       last_name: Faker::Name.unique.last_name,
-                      # email: Faker::Internet.unique.email,
-                      email: "#{:first_name}@gmail.com",
-                      password: 'topsecret',
-                      password_confirmation: 'topsecret'
+                      email: "#{f_name.downcase}@gmail.com",
+                      password: 'topsecret'
 
   2.times do
-    Tour.create! :title => 'Awesome Title', :description => 'Awesome description', :duration => rand(30..90), :max_spots => rand(1..100), :price_per_person => rand(10..100), :date => Date.tomorrow, :category_id => categories[rand(0..2)].id, :language => languages[rand(0..3)], :location => locations[rand(0..4)], :user_id => user.id
+    location = locations[rand(0..4)]
+    category = categories[rand(0..2)]
+    title = "A #{category.title.downcase} tour of #{location}"
+    description = "Explore the heart of #{location} with #{user.first_name}... #{category.description}"
+    Tour.create! title: title, description: description, duration: rand(30..90), max_spots: rand(1..100), price_per_person: rand(10..100), date: Date.tomorrow, category: category, language: languages[rand(0..3)], location: location, user: user
   end
 end
 
