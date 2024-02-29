ae0e2b3e08764a8963fadb3895e48d4e7353f522
Teodor Rothman <106080782+teorothman@users.noreply.github.com>
2024-03-01 00:34:33 +0100

    removed console logs

--- app/javascript/controllers/search_bar_controller.js
+++ app/javascript/controllers/search_bar_controller.js
@@ -4,17 +4,14 @@ export default class extends Controller {
   static targets = ["form", "search", "list"]
 
   search() {
-    console.log("Search triggered"); // Check if the method is called
     fetch(`${this.formTarget.action}?query=${encodeURIComponent(this.searchTarget.value)}`, {
       method: "GET",
       headers: { "Accept": "application/json" },
     })
     .then(response => {
-      console.log("Got response", response);
       return response.json();
     })
     .then(data => {
-      console.log("Data received", data); // Inspect the received data
       this.listTarget.innerHTML = data.tours;
     })
     .catch(error => console.error("Error:", error));
