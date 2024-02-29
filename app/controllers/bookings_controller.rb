a5af208548eb4a00e1c8ee51b209e881db7b12a5
Leo Viguier <leo.viguier@essec.edu>
2024-02-29 17:32:05 +0100

    booking section and button in footer

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,25 @@
 class BookingsController < ApplicationController
-
   def new
+    @tour = Tour.find(params[:tour_id])
     @booking = Booking.new
   end
 
   def create
-    @booking = Booking.new(booking_params[:tour_id])
-    @booking.save!
-    redirect_to "/tours/:tour_id/bookings/:id"
+    @booking = Booking.new(booking_params)
+    @tour = Tour.find(params[:tour_id])
+    @booking.tour = @tour
+    @booking.user = current_user
+    if @booking.is_private
+      @booking.total_price = @tour.price_per_person * (@tour.max_spots - 1)
+    else
+      @booking.total_price = @tour.price_per_person * @booking.nb_of_people.to_i
+    end
+
+    if @booking.save
+      redirect_to root_path, notice: 'Booking was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -16,6 +28,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    require.params(:booking).permit(:tour_id, :is_private)
+    params.require(:booking).permit(:tour_id, :is_private, :nb_of_people)
   end
 end
